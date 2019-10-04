1fbd9fba83434110d35ccf1851c72ed695e6585b
YouuInKeun <keun0390@naver.com>
2019-10-04 21:01:04 +0900

    [DELETE] commit mistake

--- TSP Project/greedy_alg_ver2/galg.py
+++
@@ -1,9 +0,0 @@
-from abc import *
-class galg(metaclass=ABCMeta):
-    @abstractmethod
-    def getPath(self):
-        pass
-
-    @abstractmethod
-    def getCost(self):
-        pass
--- TSP Project/greedy_alg_ver2/test.py
+++
@@ -1,6 +0,0 @@
-from randg import randg
-from galg import galg
-
-if __name__ == "__main__":
-    test = randg("xit2924.txt")
-    test.greedy()
